f2a86c76c7ce38ebfeb6ea4a16b5dacd6c942afe
Jens Steube <jens.steube@gmail.com>
2019-06-08 14:22:58 +0200

    Improve performance of permute.c by using an output buffer

--- src/permute.c
+++ src/permute.c
@@ -18,6 +18,54 @@
  * Credits.....: This program is using the awesome "Countdown QuickPerm Algorithm" developed by Phillip Paul Fuchs
  */
 
+#define LINE_SIZE  8192
+#define OUT_SIZE   8192
+
+typedef struct out
+{
+  FILE *fp;
+
+  char  buf[OUT_SIZE];
+  int   len;
+
+} out_t;
+
+static void out_flush (out_t *out)
+{
+  if (out->len == 0) return;
+
+  fwrite (out->buf, 1, out->len, out->fp);
+
+  out->len = 0;
+}
+
+static void out_push (out_t *out, const char *pw_buf, const int pw_len)
+{
+  char *ptr = out->buf + out->len;
+
+  memcpy (ptr, pw_buf, pw_len);
+
+  #if defined (_WIN)
+
+  ptr[pw_len + 0] = '\r';
+  ptr[pw_len + 1] = '\n';
+
+  out->len += pw_len + 2;
+
+  #else
+
+  ptr[pw_len] = '\n';
+
+  out->len += pw_len + 1;
+
+  #endif
+
+  if (out->len >= OUT_SIZE - 300)
+  {
+    out_flush (out);
+  }
+}
+
 size_t next_permutation (char *word, int *p, int k)
 {
   p[k]--;
@@ -48,17 +96,22 @@ int main (int argc, char *argv[])
   _setmode (_fileno (stdin), _O_BINARY);
   #endif
 
-  char line_buf[BUFSIZ];
+  out_t out;
+
+  out.fp  = stdout;
+  out.len = 0;
+
+  char line_buf[LINE_SIZE];
 
   int line_len;
 
-  while ((line_len = fgetl (stdin, BUFSIZ, line_buf)) != -1)
+  while ((line_len = fgetl (stdin, LINE_SIZE, line_buf)) != -1)
   {
     if (line_len == 0) continue;
 
     /* init permutation */
 
-    int p[BUFSIZ];
+    int p[LINE_SIZE];
 
     int k;
 
@@ -68,11 +121,16 @@ int main (int argc, char *argv[])
 
     /* run permutation */
 
-    puts (line_buf);
+    out_push (&out, line_buf, line_len);
 
-    while ((k = next_permutation (line_buf, p, k)) != line_len) puts (line_buf);
+    while ((k = next_permutation (line_buf, p, k)) != line_len)
+    {
+      out_push (&out, line_buf, line_len);
+    }
 
-    puts (line_buf);
+    out_push (&out, line_buf, line_len);
+
+    out_flush (&out);
   }
 
   return (0);

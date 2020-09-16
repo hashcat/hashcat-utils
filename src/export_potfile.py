0fc6b93d69505459f85d5591d693e6ba58685f6f
Rob Fuller <679319+mubix@users.noreply.github.com>
2020-09-16 12:49:16 -0400

    print actual represented btes
    simple fix to print the bytes when they are control characters like `\r`

--- src/export_potfile.py
+++ src/export_potfile.py
@@ -10,4 +10,4 @@ for line in sys.stdin:
     if passw.startswith('$HEX['):
       hexpassw = re.search('\$HEX\[([a-zA-Z0-9]+)\]', passw).group(1)
       passw = bytearray.fromhex(hexpassw).decode('latin-1')
-    print(passw)
+    print(repr(passw)[1:][:-1])

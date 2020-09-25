0be447d48f08846ca4dc85917c3e559997879465
Rob Fuller <679319+mubix@users.noreply.github.com>
2020-09-25 19:36:56 -0400

    commit the correct code

--- src/export_potfile.py
+++ src/export_potfile.py
@@ -10,8 +10,8 @@ for line in sys.stdin:
     if passw.startswith('$HEX['):
       hexpassw = re.search('\$HEX\[([a-zA-Z0-9]+)\]', passw).group(1)
       passw = bytearray.fromhex(hexpassw).decode('latin-1')
+    rep = repr(passw)
     if rep.startswith('u'):
       print(rep[2:][:-1])
     else:
       print(rep[1:][:-1])
-
